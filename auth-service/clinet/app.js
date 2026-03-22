23f86bfa0a0f1e0d82c36ca27457f5d561885da1
faghaarabi <faghaarabi@my.bcit.ca>
2026-03-21 20:49:17 -0700

    Connect frontend to deployed backend

--- auth-service/clinet/app.js
+++ auth-service/clinet/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:4000";
+const API_BASE = "https://ai-quiz-generator-2-hk2a.onrender.com";
 
 const registerForm = document.getElementById("registerForm");
 if (registerForm) {
