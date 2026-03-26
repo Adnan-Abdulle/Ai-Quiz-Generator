e32dc246cd105584c4ae9448c1e61a0c2abe10ed
faghaarabi <faghaarabi@my.bcit.ca>
2026-03-25 19:06:18 -0700

    fix(auth): use production frontend URL for reset password link

--- AI-QUIZ-GENERATOR/auth-service/clinet/app.js
+++ AI-QUIZ-GENERATOR/auth-service/clinet/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://ai-quiz-generator-3-avjw.onrender.com";
+const API_BASE = "https://ai-quiz-generator-2-hk2a.onrender.com";
 
 const registerForm = document.getElementById("registerForm");
 if (registerForm) {
