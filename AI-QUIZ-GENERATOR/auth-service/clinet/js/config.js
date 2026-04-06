5ef93b680e5491a0896f96b815c79ae35ca90042
faghaarabi <faghaarabi@my.bcit.ca>
2026-04-05 18:34:57 -0700

    Fix API_BASE

--- AI-QUIZ-GENERATOR/auth-service/clinet/js/config.js
+++ AI-QUIZ-GENERATOR/auth-service/clinet/js/config.js
@@ -1,2 +1,2 @@
-// const API_BASE = "https://ai-quiz-generator-2-hk2a.onrender.com";
-const API_BASE = "http://localhost:4000";
+const API_BASE = "https://ai-quiz-generator-2-hk2a.onrender.com";
+// const API_BASE = "http://localhost:4000";
