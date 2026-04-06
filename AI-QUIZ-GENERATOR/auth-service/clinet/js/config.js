d00d88e2ba052108fcfd2979c6aa38f9fb1ed9b5
faghaarabi <faghaarabi@my.bcit.ca>
2026-04-05 22:53:00 -0700

    change deployment service

--- AI-QUIZ-GENERATOR/auth-service/clinet/js/config.js
+++ AI-QUIZ-GENERATOR/auth-service/clinet/js/config.js
@@ -1,3 +1,3 @@
-//const API_BASE = "https://ai-quiz-generator-2-hk2a.onrender.com";
+const API_BASE = "https://ai-quiz-generator-2-hk2a.onrender.com";
 // const API_BASE = "http://localhost:4000";
-const API_BASE = "https://ai-quiz-generator-production-811e.up.railway.app/";
+// const API_BASE = "https://ai-quiz-generator-production-811e.up.railway.app/";
